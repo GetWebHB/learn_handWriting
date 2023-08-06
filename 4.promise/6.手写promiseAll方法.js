7ac8e5834d4d76cca36e610966439daf87962579
GetWebHB <1597834867@qq.com>
2023-08-06 19:19:27 +0800

    feat: promiseAll error

--- 4.promise/6.手写promiseAll方法.js
+++ 4.promise/6.手写promiseAll方法.js
@@ -10,30 +10,40 @@ const p2 = new Promise((resolve, reject) => {
 });
 const p3 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    resolve(3000);
+    reject(3000);
   }, 3000);
 });
 
-// Promise.all([p1, p2, p3]).then((res) => {
-//   console.log(res);
-// });
+// Promise.all([p1, p2, p3])
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((e) => {
+//     console.log("error:", e);
+//   });
 
 class IPromise {
   static all(promises) {
     const result = [];
     return new Promise((resolve, reject) => {
       promises.forEach((promise, idx) => {
-        promise.then((res) => {
-          result.push(res);
-          if (idx === promises.length - 1) {
-            resolve(result);
-          }
-        });
+        promise
+          .then((res) => {
+            result.push(res);
+            if (idx === promises.length - 1) {
+              resolve(result);
+            }
+          })
+          .catch((e) => {
+            reject(e);
+          });
       });
     });
   }
 }
 
-IPromise.all([p1, p2, p3]).then((res) => {
-  console.log(res);
-});
+IPromise.all([p1, p2, p3])
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((e) => console.log("err:", e));
