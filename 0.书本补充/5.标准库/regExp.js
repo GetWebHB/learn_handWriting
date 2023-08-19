08105372ee9f164fa41e1b01b95c6b6d8bd42ea3
GetWebHB <1597834867@qq.com>
2023-08-19 15:47:56 +0800

    feat: reg

--- 0.书本补充/5.标准库/regExp.js
+++ 0.书本补充/5.标准库/regExp.js
@@ -138,3 +138,82 @@
 
 // console.log(r1.test("b")); // false
 // console.log(r2.test("b")); // true
+
+// 不要给过大的范围的使用连字符 会产生你意想不到的结果
+// const reg = /[A-z]/;
+// console.log(reg.test("B"));
+// console.log(reg.test("//"));
+// console.log(reg.test("\\"));
+
+/*
+  预定义模式：某些模式的简写形式
+  \d 匹配0-9之间的任一数字，相当于[0-9]。
+  \D 匹配所有0-9以外的字符，相当于[^0-9]。
+  \w 匹配任意的字母、数字和下划线，相当于[A-Za-z0-9_]。
+  \W 除所有字母、数字和下划线以外的字符，相当于[^A-Za-z0-9_]。
+  \s 匹配空格（包括换行符、制表符、空格符等），相等于[ \t\r\n\v\f]。
+  \S 匹配非空格的字符，相当于[^ \t\r\n\v\f]。
+  \b 匹配词的边界。
+  \B 匹配非词边界，即在词的内部。
+*/
+
+// /s 案例
+// const s = /\s\w*/;
+// console.log(s.exec("hi ice"));
+
+// /b 案例
+// const s2 = /\bword/;
+// console.log(s2.test("-word"));
+// console.log(s2.test("hiword"));
+
+// 遇见换行符会终止匹配 // 使用连字符[/s/S]
+// const html = "<b>Hello</b>\n<i>world!</i>";
+// // const reg = /.*/;
+// const reg = /[\S\s]*/;
+// console.log(reg.exec(html));
+
+/*
+  (6): 重复类 {n,} 大于n次
+        {n,m} >n <m
+*/
+
+// const r1 = /lo{2}k/;
+// const r2 = /lo{2,5}k/;
+// console.log(r1.test("loooook"));
+// console.log(r2.test("loooook"));
+
+/*
+  (7): 量词符
+  ? 问号表示某个模式出现0次或1次，等同于{0, 1}。
+  * 星号表示某个模式出现0次或多次，等同于{0,}。
+  + 加号表示某个模式出现1次或多次，等同于{1,}。
+*/
+
+/*
+  (8): 贪婪模式
+  三个量词都是最大匹配
+
+  +?：表示某个模式出现1次或多次，匹配时采用非贪婪模式。
+  *?：表示某个模式出现0次或多次，匹配时采用非贪婪模式。
+  ??：表格某个模式出现0次或1次，匹配时采用非贪婪模式。
+*/
+
+// const r1 = /a+/;
+// const r2 = /a+?/; // 非贪婪模式 尽可能小匹配
+// console.log(r1.exec("aaaaaaa"));
+// console.log(r2.exec("aaaaaaa"));
+
+/*
+  (8): 修饰符
+  /g 全局 可以多次reg.test
+  /i 忽略大小写
+  /m 匹配多行
+*/
+
+// console.log(/abc/.test("ABC"));
+// console.log(/abc/i.test("ABC"));
+
+const r1 = /world$/;
+const r2 = /world$/m;
+console.log(r1.test("world\n"));
+console.log(r2.test("world\n"));
