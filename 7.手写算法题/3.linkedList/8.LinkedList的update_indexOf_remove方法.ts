4efae2fa0fd62af0ce574dc33f0d306eb7a15f8a
GetWebHB <1597834867@qq.com>
2023-09-23 16:14:46 +0800

    fix: remoteAt bug

--- 7.手写算法题/3.linkedList/8.LinkedList的update_indexOf_remove方法.ts
+++ 7.手写算法题/3.linkedList/8.LinkedList的update_indexOf_remove方法.ts
@@ -1,41 +1,41 @@
 class Node<T = any> {
-  next: Node<T> | null = null;
+  next: Node<T> | null = null
 
   constructor(public value: T) {}
 }
 
 export class LinkedList<T> {
-  size = 0;
-  head: Node<T> | null = null;
+  size = 0
+  head: Node<T> | null = null
 
   append(ele: T) {
-    const newNode = new Node<T>(ele);
+    const newNode = new Node<T>(ele)
 
     // 如果没有头/那么新创建的就是head
     if (!this.head) {
-      this.head = newNode;
+      this.head = newNode
     } else {
       // 存在head节点
-      let current = this.head;
+      let current = this.head
       while (current.next) {
-        current = current.next;
+        current = current.next
       }
       // 最后一个节点
-      current.next = newNode;
+      current.next = newNode
     }
 
-    this.size++;
+    this.size++
   }
 
   insert(position: number, ele: T): boolean {
-    if (position < 0 || position > this.size) return false;
-    let current = this.head;
-    const newNode = new Node(ele);
+    if (position < 0 || position > this.size) return false
+    let current = this.head
+    const newNode = new Node(ele)
 
     // 插入0的位置
     if (!position) {
-      newNode.next = current;
-      this.head = newNode;
+      newNode.next = current
+      this.head = newNode
       // this.head = newNode;
     } else {
       /*
@@ -45,114 +45,115 @@ export class LinkedList<T> {
           2.newNode.next = post
           3.perv.next = newNode
       */
-      const current = this.getNode(position);
-      const prev = this.getNode(position - 1);
+      const current = this.getNode(position)
+      const prev = this.getNode(position - 1)
 
-      prev!.next = newNode;
-      newNode.next = current;
+      prev!.next = newNode
+      newNode.next = current
     }
 
-    this.size++;
+    this.size++
 
-    return true;
+    return true
   }
 
-  removeAt(position: number) {
-    if (position < 0 || position >= this.size) return false;
-    let current = this.head;
+  removeAt(position: number): T | null {
+    if (position < 0 || position >= this.size) return null
+    let current = this.head
 
     if (!position) {
       // 删除head
-      this.head = current!.next;
+      this.head = current!.next
     } else {
-      const prev = this.getNode(position - 1);
-
-      prev!.next = prev!.next!.next;
+      const prev = this.getNode(position - 1)
+      current = prev!.next
+      prev!.next = prev!.next!.next
     }
+    this.size--
 
-    this.size--;
-    return true;
+    return current?.value ?? null
   }
 
   get(position: number) {
-    if (position < 0 || position >= this.size) return false;
-    const current = this.getNode(position);
+    if (position < 0 || position >= this.size) return false
+    const current = this.getNode(position)
 
-    return current?.value;
+    return current?.value
   }
 
   getNode(position: number) {
-    let idx = 0;
-    let current = this.head;
+    let idx = 0
+    let current = this.head
 
     while (idx++ < position) {
-      current = current!.next;
+      current = current!.next
     }
 
-    return current;
+    return current
   }
 
   traverse() {
-    const values: T[] = [];
+    const values: T[] = []
 
-    let current = this.head;
+    let current = this.head
 
     while (current) {
-      values.push(current.value);
-      current = current.next;
+      values.push(current.value)
+      current = current.next
     }
 
-    return values.join(" -> ");
+    return values.join(' -> ')
   }
 
   update(position: number, value: T) {
-    if (position < 0 || position >= this.size) return false;
-    const current = this.getNode(position);
+    if (position < 0 || position >= this.size) return false
+    const current = this.getNode(position)
 
-    current!.value = value;
+    current!.value = value
   }
 
   indexOf(value: T) {
-    let idx = 0;
-    let current = this.head;
+    let idx = 0
+    let current = this.head
 
     while (idx < this.size) {
       if (value === current?.value) {
-        return idx;
+        return idx
       }
 
-      idx++;
-      current = current!.next;
+      idx++
+      current = current!.next
     }
 
-    return -1;
+    return -1
   }
 
   remove(value: T) {
-    const idx = this.indexOf(value);
+    const idx = this.indexOf(value)
 
     if (idx !== -1) {
-      return this.removeAt(idx);
+      return this.removeAt(idx)
     }
   }
 }
 
-const linkedList = new LinkedList<string>();
+const linkedList = new LinkedList<string>()
 
-// linkedList.append("ice");
-// linkedList.append("panda");
-// linkedList.append("quezz");
-// console.log(linkedList.traverse());
+linkedList.append('ice')
+linkedList.append('panda')
+linkedList.append('quezz')
+console.log(linkedList.traverse())
 // console.log("------insert------");
 // linkedList.insert(0, "zhj");
 // linkedList.insert(4, "binbin");
 // linkedList.insert(2, "panpan");
 // console.log(linkedList.traverse());
 // console.log("------removeAt------");
-// linkedList.removeAt(0);
+console.log(linkedList.removeAt(0))
+console.log(linkedList.removeAt(1))
 // linkedList.removeAt(4);
 // linkedList.removeAt(2);
-// console.log(linkedList.traverse());
+console.log(linkedList.traverse())
 // console.log(linkedList.get(2));
 // console.log(linkedList.get(3));
 // console.log("------update------");
@@ -164,4 +165,4 @@ const linkedList = new LinkedList<string>();
 // console.log(linkedList.remove("q"));
 // console.log(linkedList.traverse());
 
-export {};
+export {}
