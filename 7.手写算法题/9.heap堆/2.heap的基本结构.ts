d889b5ee608b9d96d6d6f828dd4874527c21b722
GetWebHB <1597834867@qq.com>
2023-09-25 23:09:03 +0800

    feat: heap insert

---
+++ 7.手写算法题/9.heap堆/2.heap的基本结构.ts
@@ -0,0 +1,6 @@
+class Heap<T> {
+  private data: T[] = []
+  private length: number = 0
+}
+
+const heap = new Heap()
---
+++ 7.手写算法题/9.heap堆/3.heap的结构封装.ts
@@ -0,0 +1,55 @@
+class Heap<T> {
+  data: T[] = []
+  private length: number = 0
+  // 交换
+  swap(x: number, y: number) {
+    let temp = this.data[x]
+
+    this.data[x] = this.data[y]
+    this.data[y] = temp
+  }
+
+  insert(ele: T) {
+    this.data.push(ele)
+    this.length++
+
+    let index = this.length - 1
+
+    while (index > 0) {
+      let parentIdx = Math.floor((index - 1) / 2)
+      const flag = this.data[index] > this.data[parentIdx]
+      if (!flag) break
+      // 如果插入的元素 > 父元素 需要进行上滤操作
+      if (flag) {
+        this.swap(index, parentIdx)
+        index = parentIdx
+      }
+    }
+  }
+
+  extract() {}
+
+  peek() {}
+
+  get size() {
+    return this.length
+  }
+
+  isEmpty() {
+    return !this.length
+  }
+
+  buildHeap() {}
+}
+
+const heap = new Heap<number>()
+
+heap.insert(40)
+heap.insert(30)
+heap.insert(35)
+heap.insert(20)
+heap.insert(50)
+heap.insert(21)
+heap.insert(20)
+
+console.log(heap.data)
